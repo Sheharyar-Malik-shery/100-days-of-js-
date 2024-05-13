2e8ecef5b4e2976e7474959a6a7b41c1e903e91e
Sheharyar <Sheharyarmalik2019@gmail.com>
2024-05-13 22:54:45 +0500

    find the factorial of n numbers by two methods

---
+++ Factorial-of-n.js
@@ -0,0 +1,27 @@
+function findfactorial(num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    return num * findfactorial(num - 1);
+  }
+}
+console.log(findfactorial(3));
+
+// find factorial of number n (n means any nubmber) solution 01
+// let counter = 1;
+// let arr = [];
+// function findfactorial(num) {
+//   if (counter > num) {
+//     return;
+//   }
+//   console.log("the iteration is ", counter);
+//   arr.push(counter);
+//   console.log(arr);
+//   counter++;
+//   findfactorial(num);
+// }
+
+// console.log(findfactorial(4));
+// console.log(arr.reduce((a, b) => a * b));
+
+//solution 01
