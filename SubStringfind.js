45c4296acef18f33d7587c4ba3b631b671456bbe
Sheharyar <Sheharyarmalik2019@gmail.com>
2024-05-25 22:29:27 +0500

    Find the given string start with the specific string

---
+++ SubStringfind.js
@@ -0,0 +1,13 @@
+//writea function that start with the specific string
+
+// function findFirstSubString(string, substr) {
+//   let newstr = string.split(" ");
+//   newstr[0] === substr ? console.log(true) : console.log(false);
+// }
+
+//method 02
+
+// function startsWithSubstring(str, substr) {
+//   return str.slice(0, substr.length) === substr;
+// }
+console.log(startsWithSubstring("Hello World", "hello"));
